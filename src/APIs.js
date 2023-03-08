e3405fe31acceff38ee36f8131ae8dc035fa63fa
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-03-08 08:35:33 -0300

    cleaning API`s code

--- src/APIs.js
+++ src/APIs.js
@@ -1,6 +1,4 @@
 import useAxiosFetch from './hooks/useAxiosFetch'
-import axios from 'axios'
-
 
 const useGetMovies = () => {
     const { data, fetchError, isLoading } = useAxiosFetch("https://mock-api.driven.com.br/api/v8/cineflex/movies", true)
@@ -20,16 +18,6 @@ const useGetSeatList = (id) => {
 const useReserveSeat = (params) => {
     const { data, fetchError, isLoading } = useAxiosFetch("https://mock-api.driven.com.br/api/v8/cineflex/seats/book-many", false, params)
     return { data, fetchError, isLoading }
-
-/*     try {
-        const response = await axios.post("https://mock-api.driven.com.br/api/v8/cineflex/seats/book-many", params)
-        console.log(response)
-        return response
-    }
-    catch (err) {
-        console.log(err)
-        return err
-    } */
 }
 
 export { useGetMovies, useGetMovieSchedule, useGetSeatList, useReserveSeat }
